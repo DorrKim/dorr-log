0f423acd72d240d3e43f5420f035e4143657cd16
Kim hyunseok <dorrdorr9311@gmail.com>
2022-04-13 17:57:24 +0900

    config: tailwindcss poppins 폰트 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
         <title>Dorr log</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
+      <body className="font-poppins">
         <Main />
         <NextScript />
       </body>

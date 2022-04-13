e4e81ef349ee33f87b88ec82155abaa7258b9e39
Kim hyunseok <dorrdorr9311@gmail.com>
2022-04-13 17:54:15 +0900

    feat: custom _document 페이지 초기 작성 및 poppin 폰트 적용

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Dorr log</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
